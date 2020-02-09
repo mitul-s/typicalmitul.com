6f61586783899241bc081dd5dd4d7db6e303573a
mitul <mitul@mituls-mbp.lan>
2020-02-09 18:54:16 -0500

    :)

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,11 @@
-// Sticky header on scroll
-$(document).scroll(function () {
-  if ($(window).scrollTop() > $('.hero').height()) {
-    $('body').addClass('fixed-nav');
-  } else {
-    $('body').removeClass('fixed-nav');
-  }
-});
+// // Sticky header on scroll
+// $(document).scroll(function () {
+//   if ($(window).scrollTop() > $('.hero').height()) {
+//     $('body').addClass('fixed-nav');
+//   } else {
+//     $('body').removeClass('fixed-nav');
+//   }
+// });
 
 const brands = [
   { image: "/images/logos/shopify_logo.svg" },
@@ -26,19 +26,34 @@ brandsList.innerHTML = `
 `
 
 
+// const photoGrid = [
+//   { image: "/images/grid/karen.jpg" },
+//   { image: "/images/grid/rookery.jpg" },
+//   { image: "/images/grid/shangri.jpg" },
+//   { image: "/images/grid/yeshua.jpg" },
+//   { image: "/images/grid/rookery.jpg" },
+//   { image: "/images/grid/shangri.jpg" },
+//   { image: "/images/grid/karen.jpg" },
+//   { image: "/images/grid/yeshua.jpg" },
+//   { image: "/images/grid/shangri.jpg" },
+//   { image: "/images/grid/karen.jpg" },
+//   { image: "/images/grid/rookery.jpg" },
+//   { image: "/images/grid/yeshua.jpg" }
+// ]
+
 const photoGrid = [
-  { image: "/images/grid/karen.jpg" },
-  { image: "/images/grid/rookery.jpg" },
-  { image: "/images/grid/shangri.jpg" },
-  { image: "/images/grid/yeshua.jpg" },
-  { image: "/images/grid/rookery.jpg" },
-  { image: "/images/grid/shangri.jpg" },
-  { image: "/images/grid/karen.jpg" },
-  { image: "/images/grid/yeshua.jpg" },
-  { image: "/images/grid/shangri.jpg" },
-  { image: "/images/grid/karen.jpg" },
-  { image: "/images/grid/rookery.jpg" },
-  { image: "/images/grid/yeshua.jpg" }
+  { image: "/images/1.jpg" },
+  { image: "/images/12.jpg" },
+  { image: "/images/3.jpg" },
+  { image: "/images/4.jpg" },
+  { image: "/images/5.jpg" },
+  { image: "/images/6.jpg" },
+  { image: "/images/7.jpg" },
+  { image: "/images/8.jpg" },
+  { image: "/images/9.jpg" },
+  { image: "/images/10.jpg" },
+  { image: "/images/11.jpg" },
+  { image: "/images/12.jpg" }
 ]
 
 const photos = document.querySelector('.photos');
@@ -51,6 +66,21 @@ photos.innerHTML = `
     </div>
     `).join('')}
 `
+
+// Image Protection //
+// Select all displayed photos
+const image = document.querySelectorAll(".grid-photo");
+var snackbar = document.getElementById('snackbar');
+
+// Loop through nodeList and disable right click & drag
+for (let i = 0; i < image.length; i++) {
+  image[i].setAttribute("draggable", false);
+  image[i].addEventListener("contextmenu", function (event) {
+    snackbar.className = "show";
+    setTimeout(function () { snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+    event.preventDefault();
+  });
+}
 
 
 const photoGrid2 = [
@@ -78,4 +108,7 @@ photos2.innerHTML = `
 
 //     $('.hero-content').css('background-image', 'url(/images/hero/' + currentIndex++ + '.jpg)');
 // }, 5000);
+
+
+
 
