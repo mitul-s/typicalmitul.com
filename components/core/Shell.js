bbb3d2a5c2441825c758fcfe3fe9d427c59122c2
Mitul Shah <mitulxshah@gmail.com>
2021-12-05 01:26:57 -0500

    Set up new shell component (might change)

--- components/core/Shell.js
+++ components/core/Shell.js
@@ -1,11 +1,36 @@
-import { Box, Grid } from "@chakra-ui/react";
+import { HStack, Flex, Box, Link, Grid } from "@chakra-ui/react";
+
+const Navigation = () => {
+  return (
+    <Box border="1px solid black" my={2}>
+    <HStack
+      justifyContent="end"
+      width="full"
+      spacing={4}
+      lineHeight={0}
+      height="64px"
+      letterSpacing={-0.5}
+    >
+      <Link textTransform="uppercase" fontSize={18}>
+        Home
+      </Link>
+      <Link textTransform="uppercase" fontSize={18}>
+        About
+      </Link>
+      <Link textTransform="uppercase" fontSize={18}>
+        Contact
+      </Link>
+    </HStack>
+    </Box>
+  );
+};
 
 export default function Shell({ children }) {
     return (
-      <Grid as="main" gridTemplateColumns="1fr 1fr 1fr">
-        <Box>This is a navigation</Box>
+      <Box height="100%" padding={4}>
+        <Navigation />
         {children}
-        <Box>This is a footer</Box>
-      </Grid>
+        {/* <Box height="10%">This is a footer</Box> */}
+      </Box>
     );
 }
