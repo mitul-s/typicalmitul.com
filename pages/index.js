166bc4718672639b959449eecfb35fa2d3b592ce
Mitul Shah <mitulxshah@gmail.com>
2022-02-26 17:21:20 -0500

    cleanup + soem stuff

--- pages/index.js
+++ pages/index.js
@@ -1,66 +1,117 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-
-const Navgiation = () => {
-  return (
-    <nav className="flex items-center justify-between w-full px-4 py-4 font-medium uppercase bg-white border-b">
-      <a className="uppercase">Typical Mitul</a>
-      <ul className="flex gap-x-4">
-        <li>
-          <a href="/">Home</a>
-        </li>
-        <li>
-          <a href="/about">About</a>
-        </li>
-        <li>
-          <a href="/contact">Contact</a>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+import { Navigation, ButtonLink, Heading, Marquee, Section } from '@/components/index';
+import NextLink from "next/link";
 
 const Hero = () => {
   return (
-    <div className="h-[85%] border border-black rounded">
-      <div className="h-[90%] p-2">
+    <div className="h-[85%] border border-black bg-yolk rounded grid">
+      <div className="h-[90%] p-2 content">
         <div className="h-full rounded-sm bg-gray-300 border border-[#cbd6e0]"></div>
       </div>
-      <h2 className="font-bold text-center text-7xl">CHASING LIFE</h2>
-    </div>
-  );
-}
-
-
-const Marquee = () => {
-  return (
-    <div className="flex items-center justify-center h-full py-1.5 uppercase rounded bg-yolk overflow-hidden">
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
-      <span className='ml-2 whitespace-nowrap'>Available for work</span>
+      <h2 className="text-center place-self-center overlay text-[144px] font-[900] leading-none tracking-tighter">
+        CHASING LIFE
+      </h2>
     </div>
   );
 }
 
 const About = () => {
   return (
-    <div className="grid grid-cols-2">
-      <div>
-        <h3>About</h3>
-        <p>lorem10</p>
-        <button className='w-full px-4 py-2 text-left rounded bg-yolk'>Get to know me</button>
+    <div className="grid h-full grid-cols-2 py-6 gap-x-12">
+      <div className="flex flex-col justify-between">
+        <h3 className="text-3xl text-black/50">About</h3>
+        <div>
+          <p className="mb-8 text-4xl tracking-tight">
+            So I started from scratch. I made another, then another. And by the
+            end of the semester, by like box number five, I had built this
+            thing. You should have seen it. It was insane. I mean, I built it
+            out of Peruvian walnut with inlaid zebrawood.
+          </p>
+          <NextLink href="/about" passHref>
+            <ButtonLink>
+              <span>Get to know me or something</span>
+              <span>-&gt;</span>
+            </ButtonLink>
+          </NextLink>
+        </div>
       </div>
       <div>
-        <div className='w-full h-full bg-black rounded'></div>
+        <div className="w-full h-full transition duration-1000 rounded shadow-none hover:scale-[1.003] hover:-translate-y-0.5 bg-black/25 hover:shadow-xl hover:shadow-yolk/25"></div>
+      </div>
+    </div>
+  );
+}
+
+const Photos = () => {
+  return (
+    <div className="grid h-full grid-rows-2 mb-4 gap-x-6 gap-y-2 sm:grid-cols-2">
+      <div className="rounded bg-black/25 h-96"></div>
+      <div className="rounded bg-black/25 h-96"></div>
+      <div className="flex w-full col-span-2 overflow-auto gap-x-2">
+        <div className="max-w-xs overflow-hidden rounded w-96">
+          <img
+            src="https://images.unsplash.com/photo-1645642175398-5dada10d4951?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80"
+            className="object-contain"
+            alt="photo"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+const Break = () => {
+  return (
+    <>
+      <div className="grid w-full">
+        <div className="flex justify-between px-4 mt-px -mr-px font-light cursor-default select-none text-dark/50 text-7xl overlay">
+          <span className='aspect-square'>+</span>
+          <span className='aspect-square'>+</span>
+          <span className='aspect-square'>+</span>
+          <span className='aspect-square'>+</span>
+          <span className='aspect-square'>+</span>
+        </div>
+        <hr className="self-center content border-dark/10"></hr>
+      </div>
+    </>
+  );
+}
+
+const CallToAction = () => {
+  return (
+    <div className="grid grid-cols-4 py-12 gap-x-2">
+      <div className="col-span-3 py-12 space-y-12 bg-white rounded px-11">
+        <h3 className="text-3xl font-bold uppercase">Uber</h3>
+        <blockquote className="text-5xl max-w-prose">
+          &quot;We worked with Mitul to build an exhibit featuring employee
+          stories. He provided high quality work that was on-brief and delivered
+          within a tight timeline!&quot;
+        </blockquote>
+        <p>Nicole Korb</p>
+      </div>
+      <div className="flex items-center justify-center rounded bg-yolk">
+        &rarr;
+      </div>
+    </div>
+  );
+}
+
+const ScrollArea = () => {
+  return (
+    <div className="relative grid h-full grid-cols-2 gap-4">
+      <div className="sticky w-full py-12 h-fit grow-1 basis-12 top-4">
+        <Heading as="h3">Work</Heading>
+        <p>Something about my services</p>
+      </div>
+      <div className="flex flex-col gap-y-12">
+        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
+        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
+        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
+        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
+        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
+        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
       </div>
     </div>
   );
@@ -68,25 +119,35 @@ const About = () => {
 
 export default function Home() {
   return (
-    <div className="h-full">
-      <div className="absolute w-full h-full">
-        <div className="grid-container">
-          <div className="!bg-transparent grid-item"></div>
-          <div className="grid-item"></div>
-        </div>
-      </div>
-      <div className="relative z-10">
-        <Navgiation />
-      </div>
-      <div className="relative z-10 px-4 py-2">
+    <>
+      <Section padding={false}>
+        <Navigation />
+      </Section>
+      <Section isFullHeight={true}>
         <Marquee />
-      </div>
-      <div className="relative z-10 h-full px-4">
         <Hero />
-      </div>
-      <div className="relative z-10 h-full px-4">
+      </Section>
+      <Section isFullHeight={true}>
         <About />
-      </div>
-    </div>
+      </Section>
+      <Section padding={false}>
+        <Break />
+      </Section>
+      <Section>
+        <Photos />
+        <NextLink href="/photos" passHref>
+          <ButtonLink>
+            <span>See more</span>
+            <span>-&gt;</span>
+          </ButtonLink>
+        </NextLink>
+      </Section>
+      <Section>
+        <ScrollArea />
+      </Section>
+      <Section>
+        <CallToAction />
+      </Section>
+    </>
   );
 }
