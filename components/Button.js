40a5ecc1a747ecaa15fea5764c0572ff1ac2643f
Mitul Shah <mitulxshah@gmail.com>
2022-02-26 18:48:41 -0500

    fix error for buttonlink

--- components/Button.js
+++ components/Button.js
@@ -1,13 +1,15 @@
+import React from "react"
 let baseClasses = "flex justify-between w-full px-4 py-4 font-medium text-left uppercase rounded bg-yolk";
 
-export const ButtonLink = ({ href, children }) => {
-    return ( 
-        <a href={href} className={baseClasses}>
-            {children}
-        </a>
-    )
-}
+export const ButtonLink = React.forwardRef(({ href, children }, ref) => {
+  return (
+    <a href={href} className={baseClasses} ref={ref}>
+      {children}
+    </a>
+  )
+});
 
+ButtonLink.displayName = "ButtonLink";
 
 export const Button = ({ children }) => {
   return (
