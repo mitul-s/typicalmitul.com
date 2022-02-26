f9bb2ad3a867eab73b10396f1d42d18c8cc39841
Mitul Shah <mitulxshah@gmail.com>
2022-02-26 18:20:08 -0500

    create heading component with as prop

--- components/Heading.js
+++ components/Heading.js
@@ -1,15 +1,16 @@
+// import classnames from "classnames";
+
 function Heading(props) {
- const { as: Component, children } = props;
- 
- return (
-        <Component className="title">
-            {children}
-        </Component>
-    )
+  const { as: Component, children } = props;
+//   const headingClasses = classnames({
+//     "text-2xl": true,
+//   });
+
+  return <Component className={"text-3xl text-dark"}>{children}</Component>;
 }
 
 Heading.defaultProps = {
- as: 'h2',
+  as: "h2",
 };
 
 export default Heading;
