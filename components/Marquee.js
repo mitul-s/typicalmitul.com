b93c8792bd640346b1c9ca683328d4c1f2dc6df6
Mitul Shah <mitulxshah@gmail.com>
2022-02-26 22:03:10 -0500

    clean up

--- components/Marquee.js
+++ components/Marquee.js
@@ -14,43 +14,22 @@ const marqueeVariants = {
     },
   };
 
-export const Marquee2 = () => {
+export const Marquee = () => {
  return (
    <>
-     <div className="relative w-full max-w-full py-1 my-2 overflow-x-hidden rounded bg-yolk">
+     <div className="w-full max-w-full py-1.5 my-2 overflow-x-hidden rounded bg-yolk">
        <motion.div
-         className="uppercase whitespace-nowrap"
+         className="space-x-1.5 tracking-tight uppercase cursor-default select-none whitespace-nowrap will-change-transform"
          variants={marqueeVariants}
          animate="animate"
        >
-         {[...Array(20)].map((e, i) => <span className="mx-1 tracking-tight" key={i}>Available for work</span>)}
+         {[...Array(20)].map((e, i) => (
+           <span key={i}>Available for work</span>
+         ))}
        </motion.div>
      </div>
    </>
  );
 }
-
-
-const Marquee = () => {
-  return (
-    <div className="flex py-2 overflow-hidden">
-      <div className="flex items-center justify-center h-full py-1.5 uppercase rounded bg-yolk marquee-container">
-        <div className="w-full marquee-item">
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-          <span className="whitespace-nowrap">Available for work</span>
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default Marquee;
