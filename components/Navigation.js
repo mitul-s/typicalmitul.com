19c59fe0308be11100fe804a9ce9ddb109a35cb2
Mitul Shah <mitulxshah@gmail.com>
2022-02-28 00:54:06 -0500

    add link

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,18 +1,27 @@
 import NextLink  from "next/link";
+import { Link } from "@components"
 
 const Navigation = () => {
   return (
     <nav className="flex items-center justify-between w-full px-4 py-4 font-medium uppercase border-b bg-eggshell border-dark">
-      <a className="uppercase">Typical Mitul</a>
+      <NextLink href="/" passHref>
+        <Link>Typical Mitul</Link>
+      </NextLink>
       <ul className="flex gap-x-4">
         <li>
-          <NextLink href="/">Home</NextLink>
+          <NextLink href="/" passHref>
+            <Link>Home</Link>
+          </NextLink>
         </li>
         <li>
-          <NextLink href="/about">About</NextLink>
+          <NextLink href="/about" passHref>
+            <Link>About</Link>
+          </NextLink>
         </li>
         <li>
-          <NextLink href="/contact">Contact</NextLink>
+          <NextLink href="/contact" passHref>
+            <Link>Contact</Link>
+          </NextLink>
         </li>
       </ul>
     </nav>
