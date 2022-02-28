8162535483536edfbe7a4c53d50d4e12c0063aa4
Mitul Shah <mitulxshah@gmail.com>
2022-02-28 00:53:56 -0500

    wip

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import { Navigation, ButtonLink, Heading, Marquee, Section } from '@components';
+import { Marquee2 } from '@/components/Marquee';
 import NextLink from "next/link";
 
 const Hero = () => {
@@ -17,11 +18,14 @@ const Hero = () => {
   );
 }
 
+let us =
+  "https://images.unsplash.com/photo-1645389776527-43c47e909550?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
+
 const About = () => {
   return (
-    <div className="grid h-full grid-cols-2 py-6 gap-x-12">
+    <div className="grid h-full grid-cols-2 gap-x-12">
       <div className="flex flex-col justify-between">
-        <h3 className="text-3xl text-black/50">About</h3>
+        <Heading as="h2">Work</Heading>
         <div>
           <p className="mb-8 text-4xl tracking-tight">
             So I started from scratch. I made another, then another. And by the
@@ -38,7 +42,9 @@ const About = () => {
         </div>
       </div>
       <div>
-        <div className="w-full h-full transition duration-1000 rounded shadow-none hover:scale-[1.003] hover:-translate-y-0.5 bg-black/25 hover:shadow-xl hover:shadow-yolk/25"></div>
+        <div className="w-full h-[90vh] transition duration-1000 rounded shadow-none hover:scale-[1.003] hover:-translate-y-0.5 bg-black/25 hover:shadow-xl hover:shadow-yolk/25 overflow-hidden">
+          <img src={us} alt="About" className="object-cover" />
+        </div>
       </div>
     </div>
   );
@@ -101,24 +107,38 @@ const CallToAction = () => {
 const ScrollArea = () => {
   return (
     <div className="relative grid h-full grid-cols-2 gap-4">
-      <div className="sticky top-0 w-full h-screen py-12 grow-1">
-        <div className='flex flex-col justify-between h-full'>
+      <div className="sticky top-0 w-full h-screen pt-12 pb-6 grow-1">
+        <div className="flex flex-col justify-between h-full">
           <Heading as="h3">Work</Heading>
-          <p className="text-3xl">
-            So I started from scratch. I made another, then another. And by the
-            end of the semester, by like box number five, I had built this
-            thing. You should have seen it. It was insane. I mean, I built it
-            out of Peruvian walnut with inlaid zebrawood.
-          </p>
+          <div>
+            <p className="text-3xl">
+              So I started from scratch. I made another, then another. And by
+              the end of the semester, by like box number five, I had built this
+              thing. You should have seen it. It was insane. I mean, I built it
+              out of Peruvian walnut with inlaid zebrawood.
+            </p>
+            <p className="w-2/3 mt-6 text-md text-dark/50">
+              Photography is my craft. From brand shoots to non-exclusive
+              licensing, I’m able to provide creative pieces that work for you.
+            </p>
+            <div className="flex mt-4 gap-x-4">
+              <div className="flex items-center justify-center w-1/2 px-6 py-4 border rounded border-dark">
+                Photography
+              </div>
+              <div className="flex items-center justify-center w-1/2 px-6 py-4 border rounded border-dark">
+                Licensing
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-      <div className="flex flex-col py-3 gap-y-3">
-        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
-        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
-        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
-        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
-        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
-        <div className="w-full border border-black rounded h-96 bg-slate-100"></div>
+      <div className="flex flex-col pt-12 pb-6 gap-y-3">
+        <div className="w-full border rounded shadow-sm border-stone-light h-96 bg-dark"></div>
+        <div className="w-full border rounded shadow-sm border-stone-light h-96 bg-dark"></div>
+        <div className="w-full border rounded shadow-sm border-stone-light h-96 bg-dark"></div>
+        <div className="w-full border rounded shadow-sm border-stone-light h-96 bg-dark"></div>
+        <div className="w-full border rounded shadow-sm border-stone-light h-96 bg-dark"></div>
+        <div className="w-full border rounded shadow-sm border-stone-light h-96 bg-dark"></div>
       </div>
     </div>
   );
@@ -149,11 +169,14 @@ export default function Home() {
           </ButtonLink>
         </NextLink>
       </Section>
-      <Section>
+      <Section className="relative">
         <ScrollArea />
       </Section>
       <Section>
         <CallToAction />
+      </Section>
+      <Section>
+        <Break />
       </Section>
     </>
   );
