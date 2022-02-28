a393f528e4627293dd31d795cc055d32925a232a
Mitul Shah <mitulxshah@gmail.com>
2022-02-28 00:53:49 -0500

    stone colour + border radius to tw config

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     borderRadius: {
       DEFAULT: "5px",
       sm: "4px",
+      full: "50px",
     },
     fontFamily: {
       sans: ["ABC Monument Grotesk Unlicensed Trial", "Inter", "sans-serif"],
@@ -15,6 +16,10 @@ module.exports = {
       colors: {
         yolk: "#CCFF00",
         eggshell: "#F8F8F8",
+        stone: {
+          DEFAULT: "#A4A4A4",
+          light: "#cbd6e0",
+        },
         dark: "#030303",
       },
     },
