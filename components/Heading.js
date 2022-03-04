0c91692db7dce648e91b2e626aefc73127f23c0a
Mitul Shah <mitulxshah@gmail.com>
2022-03-04 01:40:07 -0500

    style updates (unused)

--- components/Heading.js
+++ components/Heading.js
@@ -1,7 +1,7 @@
 function Heading(props) {
   const { as: Component, children } = props;
 
-  return <Component className={"text-3xl text-stone"}>{children}</Component>;
+  return <Component className={"text-2xl uppercase font-medium text-stone"}>{children}</Component>;
 }
 
 Heading.defaultProps = {
