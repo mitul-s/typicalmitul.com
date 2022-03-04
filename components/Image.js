c1b574cd901911df27a83510a431900894341911
Mitul Shah <mitulxshah@gmail.com>
2022-03-04 00:09:45 -0500

    change aspect ratio wip

--- components/Image.js
+++ components/Image.js
@@ -1,7 +1,7 @@
 import NextImage from 'next/image';
 
 const Image = ({ src, alt, ...props }) => (
-  <div className="relative w-full overflow-hidden rounded aspect-[16/10] border border-stone-light shadow transition-all hover:-translate-y-0.5 hover:scale-[1.003] hover:shadow-yolk">
+  <div className="relative w-full overflow-hidden rounded aspect-[3/2] border border-stone-light shadow transition-all hover:-translate-y-0.5 hover:scale-[1.003] hover:shadow-yolk">
     <NextImage {...props} src={src} alt={alt} layout="fill" objectFit="cover" />
   </div>
 );
