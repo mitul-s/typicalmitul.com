6cf030c421467333281914e3e1b779fd6fb9d442
Mitul Shah <mitulxshah@gmail.com>
2022-03-04 10:26:34 -0500

    wrap app with tooltip provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import { Provider } from "@radix-ui/react-tooltip";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp
