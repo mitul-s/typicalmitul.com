0a203e7e8f1c5f7f05714ba6ca6a83e3f3baa079
Mitul Shah <mitulxshah@gmail.com>
2022-03-09 00:58:59 -0500

    create sections

---
+++ components/sections/Hero.js
@@ -0,0 +1,14 @@
+const Hero = () => {
+  return (
+    <div className="grid h-full border rounded border-dark bg-yolk">
+      <div className="p-2 h-[90%] content">
+        <div className="h-full rounded-sm bg-gray-300 border border-[#cbd6e0]"></div>
+      </div>
+      <h2 className="flex justify-center items-end overlay text-[144px] font-[900] leading-none tracking-tighter">
+        CHASING LIFE
+      </h2>
+    </div>
+  );
+};
+
+export default Hero;
