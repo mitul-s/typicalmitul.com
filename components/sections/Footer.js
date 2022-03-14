f19f54344ca2ade19f1b39011293bd8a94daed60
Mitul Shah <mitulxshah@gmail.com>
2022-03-13 23:16:06 -0400

    wip animation

--- components/sections/Footer.js
+++ components/sections/Footer.js
@@ -1,11 +1,14 @@
+import { ArrowUpRight } from "phosphor-react";
 
 const Footer = () => {
   return (
     <footer className="border-t border-dark">
       <div className="flex justify-between px-4 py-2 font-medium uppercase">
-        <div className="space-x-4">
-          <a className="">Instagram</a>
-          <a className="">Twitter</a>
+        <div className="flex space-x-4">
+          <a className="flex items-center cursor-pointer group">Instagram
+          <ArrowUpRight className="-mt-0.5 transition group-hover:rotate-45" weight="bold" /></a>
+          <a className="flex items-center cursor-pointer group">Twitter
+          <ArrowUpRight className="-mt-0.5 transition group-hover:rotate-45" weight="bold" /></a>
         </div>
         <a>Contact</a>
       </div>
