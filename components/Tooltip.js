6a7d7eab0d7c721e7ccd64828f7d3e648dea6777
Mitul Shah <mitulxshah@gmail.com>
2022-03-13 23:15:32 -0400

    tweaks

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -6,10 +6,11 @@ import {
 
 const Tooltip = ({ text, children }) => {
   return (
-    <TooltipRoot delayDuration={0}>
+    <TooltipRoot delayDuration={350}>
       <TooltipTrigger>{children}</TooltipTrigger>
       <TooltipContent
         side="top"
+        sideOffset={4}
         className="text-xs px-1.5 py-1 rounded-sm shadow bg-dark text-eggshell leading-none tooltip-animation"
       >
         {text}
