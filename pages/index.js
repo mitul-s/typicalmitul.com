09e4b0155aed71755c734671f0491055357375a1
Mitul Shah <mitulxshah@gmail.com>
2022-03-13 23:16:27 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,21 +5,31 @@ import { Navigation, ButtonLink, Heading, Marquee, Section } from "@components";
 import Hero from "@/sections/Hero";
 import About from "@/sections/About";
 import Footer from "@/sections/Footer";
+import Contact from "@/sections/Contact";
+import Break from "@/sections/Break";
 import Image from "@/components/Image";
 import NextLink from "next/link";
 import NextImage from "next/image";
+import React from "react"
 
 let us =
   "https://images.unsplash.com/photo-1645389776527-43c47e909550?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=987&q=80";
-let us2 = "https://images.unsplash.com/photo-1646075900858-6e813f664593";
+let us2 =
+  "https://images.unsplash.com/photo-1647089490719-d539f4041119";
 let us3 = "https://images.unsplash.com/photo-1643068485255-15bb6c3727da";
+let ph =
+  "https://res.cloudinary.com/dcf2075hg/image/upload/v1647147195/Typical%20Mitul/Test/RainyJays_ovyisq.jpg";
+  let ph2 =
+    "https://res.cloudinary.com/dcf2075hg/image/upload/v1647147216/Typical%20Mitul/Test/Karen_ifcimd.jpg";
+    let ph3 =
+      "https://res.cloudinary.com/dcf2075hg/image/upload/v1647147132/Typical%20Mitul/Test/IMG_7893_cntower_print_men3lj.jpg";
 
 const Photos = () => {
   return (
     <>
       <div className="grid h-[90vh] mb-4 gap-x-6 gap-y-2 sm:grid-cols-2 px-4">
         <div className="relative w-full h-full border rounded shadow-sm border-stone">
-          <NextImage src={us3} layout="fill" objectFit="cover" />
+          <NextImage src={ph3} layout="fill" objectFit="cover" className="object-top"/>
         </div>
         <div className="relative w-full h-full border rounded shadow-sm border-stone">
           <NextImage src={us2} layout="fill" objectFit="cover" />
@@ -46,22 +56,6 @@ const Photos = () => {
   );
 };
 
-const Break = () => {
-  return (
-    <>
-      <div className="grid w-full">
-        <hr className="self-center content border-stone"></hr>
-        <div className="flex justify-between px-4 mt-px -mr-px font-light cursor-default select-none text-stone text-7xl overlay">
-          <span className="aspect-square bg-eggshell">+</span>
-          <span className="aspect-square bg-eggshell">+</span>
-          <span className="aspect-square bg-eggshell">+</span>
-          <span className="aspect-square bg-eggshell">+</span>
-          <span className="aspect-square bg-eggshell">+</span>
-        </div>
-      </div>
-    </>
-  );
-};
 
 const CallToAction = () => {
   return (
@@ -103,7 +97,13 @@ const ScrollArea = () => {
               Photography is my craft. From brand shoots to non-exclusive
               licensing, I’m able to provide creative pieces that work for you.
             </p>
-            <div className="flex mt-4 gap-x-4">
+            <div className="flex mt-4 gap-x-1">
+            {/* <div className="bg-dark text-yolk font-medium uppercase tracking-wide px-1.5 leading-none py-1 self-end rounded text-sm border border-yolk">
+              Photography
+            </div>
+            <div className="bg-dark text-yolk font-medium uppercase tracking-wide px-1.5 leading-none py-1 self-end rounded text-sm border border-yolk">
+              Licensing
+            </div> */}
               <div className="flex items-center justify-center w-1/2 px-6 py-4 border rounded border-dark">
                 Photography
               </div>
@@ -115,55 +115,28 @@ const ScrollArea = () => {
         </div>
       </div>
       <div className="flex flex-col pt-12 pb-6 gap-y-3">
+        <Image src={us} alt="" />
         <Image src={us2} alt="" />
+        <Image src={us3} alt="" />
+        <Image src={ph} alt="" />
+        <Image src={ph2} alt="" />
         <Image src={us2} alt="" />
-        <Image src={us2} alt="" />
-        <Image src={us2} alt="" />
-        <Image src={us2} alt="" />
-        <Image src={us2} alt="" />
-        <Image src={us2} alt="" />
+        <Image src={ph3} vertical alt="" />
       </div>
     </div>
-  );
-};
-
-const Contact = () => {
-  return (
-    <form className="flex flex-col justify-end w-full py-6 h-96">
-      <div className="w-full">
-        <label className="sr-only" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="w-full py-2 text-3xl bg-transparent border-b border-dark/25"
-          id="email"
-          name="email"
-          placeholder="What's your email?"
-        />
-      </div>
-      <div className="w-full">
-        <label className="sr-only" htmlFor="email">
-          Email
-        </label>
-        <textarea
-          className="w-full py-2 text-3xl bg-transparent border-b border-dark/25"
-          id="email"
-          name="email"
-          placeholder="What's on your mind?"
-        />
-      </div>
-      <button className="w-full py-4 rounded bg-yolk">Submit</button>
-    </form>
   );
 };
 
 export default function Home() {
   return (
     <div className="relative min-h-full">
-      <div className="absolute w-full h-full">
-        <div className="grid-container">
-          <div className="grid-item first:bg-transparent"></div>
-          <div className="grid-item"></div>
+      <div className="absolute w-full h-full px-4 pointer-events-none">
+        <div className="relative grid h-full max-w-full grid-cols-2 grid-rows-1 mx-auto my-0">
+          <div className="w-px bg-stone first-of-type:bg-transparent"></div>
+          <div className="hidden w-px bg-stone"></div>
+          <div className="w-px bg-stone"></div>
+          <div className="hidden w-px bg-stone"></div>
+          <div className="w-px bg-stone last-of-type:bg-transparent last-of-type:absolute last-of-type:top-0 last-of-type:right-0 last-of-type:h-full"></div>
         </div>
       </div>
       <Navigation />
@@ -175,20 +148,20 @@ export default function Home() {
           </div>
         </section>
         {/* <div className="w-full h-px bg-dark/50"></div> */}
-        <section className="mx-4 my-6">
+        <section className="mx-4">
           <About />
         </section>
-        <Section padding={false}>
-          <Break />
-        </Section>
+        <Break />
         <section className="min-h-full">
           <Photos />
-          <NextLink href="/photos" passHref>
-            <ButtonLink>
-              <span>See more</span>
-              <span>-&gt;</span>
-            </ButtonLink>
-          </NextLink>
+          <div className="px-4 mt-6">
+            <NextLink href="/photos" passHref>
+              <ButtonLink>
+                <span>See more</span>
+                <span>-&gt;</span>
+              </ButtonLink>
+            </NextLink>
+          </div>
         </section>
         <section className="px-4">
           <ScrollArea />
@@ -196,15 +169,13 @@ export default function Home() {
         <Section>
           <CallToAction />
         </Section>
-        <Section>
-          <Break />
-        </Section>
-        <Section>
+        <Break />
+        <section className="min-h-full">
           <Contact />
-        </Section>
-        <Section padding={false}>
+        </section>
+        <section>
           <Footer />
-        </Section>
+        </section>
       </main>
     </div>
   );
