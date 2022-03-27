131a296f5a6099568888966702740fd48298211c
Mitul Shah <mitulxshah@gmail.com>
2022-03-27 00:08:36 -0400

    tweakinnnnn

--- components/sections/Hero.js
+++ components/sections/Hero.js
@@ -1,12 +1,13 @@
 import NextImage from "next/image";
+import Marquee from "../Marquee";
 let us3 =
   "https://res.cloudinary.com/dcf2075hg/image/upload/v1647147142/Typical%20Mitul/Test/IMG_1448_mjrtnf.jpg";
 
 const Hero = () => {
   return (
-    <div className="grid h-full border rounded border-dark bg-yolk">
-      <div className="p-2 content h-[90%]">
-        <div className="relative h-full p-2 overflow-hidden rounded-sm">
+    <div className="grid h-full rounded-lg shadow bg-yolk ring-2 ring-offset-4 ring-yolk shadow-yolk">
+      <div className="h-full p-0.5 content">
+        <div className="relative h-full p-1 overflow-hidden rounded-lg">
           <NextImage
             src={us3}
             alt="me"
@@ -17,7 +18,7 @@ const Hero = () => {
           {/* <div className="h-full rounded-sm bg-gray-300 border border-[#cbd6e0]"></div> */}
         </div>
       </div>
-      <h2 className="hero-heading flex justify-center items-end overlay z-10 text-[144px] font-[900] leading-none tracking-tighter">
+      <h2 className="hero-heading flex justify-center items-center overlay z-10 text-[72px] font-[900] leading-none tracking-tighter -mt-4">
         CHASING LIFE
       </h2>
     </div>
