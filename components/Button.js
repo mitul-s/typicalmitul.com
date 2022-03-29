19dfa2fb14d6b00dda44c97662441195288ed055
Mitul Shah <mitulxshah@gmail.com>
2022-03-29 00:52:15 -0400

    wip

--- components/Button.js
+++ components/Button.js
@@ -1,23 +1,28 @@
 import React from "react"
 import { motion } from "framer-motion"
-import { ArrowUpRight } from "phosphor-react";
+import { arrowVariants } from "../constants";
+import { MotionArrowUpRight } from "../constants";
 let baseClasses =
   "inline-block w-full font-medium text-left uppercase border border-dark rounded-lg bg-yolk transition-all hover:rounded-full hover:bg-dark hover:border-yolk hover:text-white overflow-hidden";
-
 
 export const GlowButton = ({ children, ...props }) => {
   return (
     <div className="isolate">
-      <button
+      <motion.button
+        whileHover="hover"
+        animate="initial"
         className="relative flex items-center px-4 py-1 transition-all duration-1000 rounded-full shadow-md outline-none glow bg-eggshell/50 hover:bg-eggshell hover:ring-4 hover:ring-yolk focus:ring-yolk focus:ring-2 gap-x-1"
         {...props}
       >
-        <span>Open Project</span>
-        <ArrowUpRight className="shrink-0" />
-      </button>
+        <span>Visit Project</span>
+        <MotionArrowUpRight className="shrink-0" variants={arrowVariants} custom={0.8} />
+      </motion.button>
     </div>
   );
 };
+
+
+
 
 export const ButtonLink = React.forwardRef(({ href, children }, ref) => {
   return (
