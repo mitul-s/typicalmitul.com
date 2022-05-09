e54874e41b25e7ded669002c48b5e78734627d36
Mitul Shah <mitulxshah@gmail.com>
2022-05-09 00:11:41 -0400

    Create useCopyToClipboard.js

---
+++ hooks/useCopyToClipboard.js
@@ -0,0 +1,30 @@
+import { useState, useCallback, useEffect } from "react";
+
+const useCopyToClipboard = (text, notifyTimeout = 2500) => {
+  
+  const [copyStatus, setCopyStatus] = useState(null);
+  
+  const copy = useCallback(() => {
+    navigator.clipboard.writeText(text).then(
+      () => setCopyStatus(true),
+      () => setCopyStatus(false)
+    );
+  }, [text]);
+
+  useEffect(() => {
+    if (copyStatus === false) {
+      return false;
+    }
+
+    const timeoutId = setTimeout(
+      () => setCopyStatus(null),
+      notifyTimeout
+    );
+
+    return () => clearTimeout(timeoutId);
+  }, [copyStatus, notifyTimeout]);
+
+  return [copyStatus, copy];
+};
+
+export default useCopyToClipboard;
