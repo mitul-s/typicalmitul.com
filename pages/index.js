f1ee7412541d52d03c4f59fb24fbe0ec90b8b346
Mitul Shah <mitulxshah@gmail.com>
2022-08-05 23:37:34 -0400

    bring break in for contact

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
         <Work />
       </Section>
       <CallToAction />
-      {/* <Break /> */}
+      <Break />
       <section className="min-h-full">
         <Contact />
       </section>
