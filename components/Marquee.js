ce59015948c303f270f5a95c792ac6711e9148bb
Mitul Shah <mitulxshah@gmail.com>
2022-08-09 10:37:27 -0400

    remove old code

--- components/Marquee.js
+++ components/Marquee.js
@@ -1,16 +1,4 @@
-export const Marquee2 = () => {
- return (
-   <>
-     <div className="marquee">
-       <div className="marquee__inner" aria-hidden="true">
-         {[...Array(15)].map((e, i) => (
-           <span key={i}>Available for work</span>
-         ))}
-       </div>
-     </div>
-   </>
- );
-}
+
 export const Marquee = () => {
  return (
    <>
