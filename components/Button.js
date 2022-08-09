a895aa8668544b03119a6f2bbd64062894995288
Mitul Shah <mitulxshah@gmail.com>
2022-08-08 23:00:53 -0400

    tweak

--- components/Button.js
+++ components/Button.js
@@ -1,8 +1,9 @@
-import React from "react"
-import { motion } from "framer-motion"
+import React from "react";
+import { motion } from "framer-motion";
 import { arrowVariants } from "../constants";
 import { MotionArrowUpRight } from "../constants";
 import { Slot } from "@radix-ui/react-slot";
+import { cva } from "class-variance-authority";
 
 let baseClasses =
   "w-full font-medium text-left uppercase border border-dark rounded-lg bg-yolk transition-all hover:bg-dark hover:border-yolk hover:text-white overflow-hidden";
@@ -17,22 +18,77 @@ export const GlowButton = ({ children, ...props }) => {
         {...props}
       >
         <span>Visit Project</span>
-        <MotionArrowUpRight className="shrink-0" variants={arrowVariants} custom={0.8} />
+        <MotionArrowUpRight
+          className="shrink-0"
+          variants={arrowVariants}
+          custom={0.8}
+        />
       </motion.button>
     </div>
   );
 };
 
+const button = cva(
+  [
+    "button",
+  ],
+  {
+    variants: {
+      intent: {
+        primary: [
+          "bg-yolk",
+          "text-dark",
+          "border-dark",
+          "hover:bg-yolk/75",
+          "font-medium",
+        ],
+      },
+      fullWidth: {
+        true: ["w-full"]
+      },
+    },
+    defaultVariants: {
+      intent: "primary",
+    },
+  }
+);
 
-export const Button = React.forwardRef(({ asChild, children, ...props }, ref) => {
-  const Component = asChild ? Slot : "button";
-  return (
-    <Component className={`${baseClasses} flex items-center justify-between p-4 `} ref={ref} asChild={asChild} {...props}>
-      {children}
-    </Component>
-  );
-});
+export const Button = React.forwardRef(
+  ({ asAnchor, type, intent, fullWidth, className, children, ...props }, forwardRef) => {
+    const Component = asAnchor ? "a" : "button";
+    const defaultType = asAnchor ? undefined : "button";
 
-Button.displayName = "Button";
+    return (
+      <Component
+        type={type ?? defaultType}
+        className={button({ intent, fullWidth, class: className })}
+        ref={forwardRef}
+        {...props}
+      >
+        {children}
+      </Component>
+    );
+  }
+);
+
+Button.displayName = "Button2";
+
+// export const Button = React.forwardRef(
+//   ({ asChild, children, ...props }, ref) => {
+//     const Component = asChild ? Slot : "button";
+//     return (
+//       <Component
+//         className={`${baseClasses} flex items-center justify-between p-4 `}
+//         ref={ref}
+//         asChild={asChild}
+//         {...props}
+//       >
+//         {children}
+//       </Component>
+//     );
+//   }
+// );
+
+// Button.displayName = "Button";
 
 export default Button;
