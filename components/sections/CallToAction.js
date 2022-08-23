369546e6cd98fd57923e9e6b51ea32aa3c4c129b
Mitul Shah <mitulxshah@gmail.com>
2022-08-22 23:52:12 -0400

    some grainnn

--- components/sections/CallToAction.js
+++ components/sections/CallToAction.js
@@ -1,19 +1,33 @@
+import NextImage from "next/image"
+
 const CallToAction = () => {
   return (
     <div className="px-6 md:p-4">
-      <div className="px-4 py-8 border-2 rounded text-dark md:py-12 md:px-12 bg-eggshell border-yolk">
-        <div className="flex flex-col mx-auto max-w-7xl gap-y-6 md:gap-y-12">
-          <h3 className="-mt-2 text-lg font-bold uppercase md:text-3xl">
-            Uber
-          </h3>
-          <blockquote className="text-2xl !leading-tight md:text-5xl">
-            &quot;We worked with Mitul to build an exhibit featuring employee
-            stories. He provided high quality work that was on-brief and
-            delivered within a tight timeline!&quot;
+      <div
+        className="px-4 py-4 sm:py-8 rounded text-eggshell md:py-12 md:px-12 bg-[#276EF0] shadow shadow-[#276EF0]"
+        style={{
+          backgroundImage:
+            "linear-gradient(to right, #276EF0, rgba(0, 0, 0, 0)), url(https://grainy-gradients.vercel.app/noise.svg);",
+        }}
+      >
+        <div className="flex flex-col max-w-6xl gap-y-6 md:gap-y-12">
+          <div className={`relative w-12 sm:w-20 h-8 overflow-hidden`}>
+            <NextImage
+              src="/images/logos/uber.png"
+              objectFit="contain"
+              layout="fill"
+            />
+          </div>
+          <blockquote className="text-xl sm:text-2xl sm:!leading-tight md:text-5xl">
+            We worked with Mitul to build an exhibit featuring employee
+            stories. He provided{" "}
+            <b className="font-mtl-bold">high quality work</b> that was on-brief
+            and delivered{" "}
+            <b className="font-mtl-bold">within a tight timeline</b>!
           </blockquote>
           <div>
-            <p className="">Nicole Korb</p>
-            <p className="text-dark/50">Operations Manager</p>
+            <p className="text-sm">Nicole Korb</p>
+            <p className="text-sm text-eggshell/50">Operations Manager</p>
           </div>
         </div>
       </div>
