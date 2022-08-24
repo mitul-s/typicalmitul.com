f85b6b28561e46a134050bc96247fae896918b13
Mitul Shah <mitulxshah@gmail.com>
2022-08-23 22:49:04 -0400

    wrap with layout

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import PhotoGrid from "@/sections/PhotoGrid";
 import About from "@/sections/About";
 import Break from "@/sections/Break";
-import Contact from "@/sections/Contact";
+import { Contact } from "@/sections/Contact";
 import Footer from "@/sections/Footer";
 import Hero from "@/sections/Hero";
 import Work from "@/sections/Work";
 import CallToAction from "@/sections/CallToAction";
+import Layout from "@/components/Layout";
 
 import Script from "next/script";
 
@@ -18,26 +19,28 @@ export default function Home() {
   return (
     <>
       <Script src="/getHeight.js"></Script>
-      <main className="relative">
-        <Hero />
-        <Section>
-          <About />
-        </Section>
-        <Break />
-        <section className="h-auto px-6 md:px-4">
-          <PhotoGrid />
-        </section>
-        <Break />
-        <Section>
-          <Work />
-        </Section>
-        <CallToAction />
-        <Break />
-        <section className="min-h-full">
-          <Contact />
-        </section>
-        <Footer />
-      </main>
+      <Layout>
+        <main className="relative">
+          <Hero />
+          <Section>
+            <About />
+          </Section>
+          <Break />
+          <section className="h-auto px-6 md:px-4">
+            <PhotoGrid />
+          </section>
+          <Break />
+          <Section>
+            <Work />
+          </Section>
+          <CallToAction />
+          <Break />
+          <section className="min-h-full">
+            <Contact />
+          </section>
+          <Footer />
+        </main>
+      </Layout>
     </>
   );
 }
