d6b37619b58d8e9932434823e1174cb7bfcc8779
Mitul Shah <mitulxshah@gmail.com>
2022-08-25 22:51:53 -0400

    replace heading

--- components/Heading.js
+++ components/Heading.js
@@ -1,11 +1,8 @@
-function Heading(props) {
-  const { as: Component, children } = props;
-
-  return <Component className={"text-2xl uppercase font-medium text-stone"}>{children}</Component>;
-}
-
-Heading.defaultProps = {
-  as: "h2",
-};
+const Heading = ({ children }) => (
+  <div className="flex items-center my-2 leading-none uppercase gap-x-2">
+    <span className="w-2.5 h-2.5 rounded bg-yolk border border-dark -mt-0.5" />
+    <h2 className="text-dark">{children}</h2>
+  </div>
+);
 
 export default Heading;
