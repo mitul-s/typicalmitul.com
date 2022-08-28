1a97f4c2219fff4c77da4780dfc683fb04fe1603
Mitul Shah <mitulxshah@gmail.com>
2022-08-27 22:17:19 -0400

    add basic seo content

--- pages/_app.js
+++ pages/_app.js
@@ -4,8 +4,8 @@ import '../styles/globals.css'
 import React, { useEffect } from "react";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { SheetProvider } from "@/components/Sheet"
+import { DefaultSeo } from "next-seo";
 
-import NextHeadSeo from "next-head-seo";
 import splitbee from "@splitbee/web";
 
 const MyApp = ({ Component, pageProps }) => {
@@ -15,21 +15,23 @@ const MyApp = ({ Component, pageProps }) => {
   }, [])
 
   return (
-    <>
-      <NextHeadSeo
-        title="Typical Mitul – Toronto Photographer for Hire"
+    <TooltipProvider>
+      <SheetProvider>
+      <DefaultSeo
+        title={undefined}
+        titleTemplate="%s | Typical Mitul"
+        defaultTitle="Typical Mitul | Toronto Photographer for Hire"
         description="Photographer based in Toronto, Canada specializing in cityscapes, architecture and portraits."
         canonical="https://typicalmitul.com"
         twitter={{
+          handle: "@typicalmitul",
           site: "@typicalmitul",
+          cardType: "summary",
         }}
       />
-      <TooltipProvider>
-        <SheetProvider>
-          <Component {...pageProps} />
-        </SheetProvider>
-      </TooltipProvider>
-    </>
+        <Component {...pageProps} />
+      </SheetProvider>
+    </TooltipProvider>
   );
 }
 
