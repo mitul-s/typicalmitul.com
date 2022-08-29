3e55f4647a96b7249330c4183cb3d86a5f6346a3
Mitul Shah <mitulxshah@gmail.com>
2022-08-28 23:42:08 -0400

    create dialog layout

--- components/sections/PhotoGrid.js
+++ components/sections/PhotoGrid.js
@@ -8,6 +8,12 @@ import grid_light from "../../public/images/grid_light.jpg"
 import cn_shangri from "../../public/images/cn_shangri.jpg";
 import jay_lookup from "../../public/images/jay_lookup.jpg";
 import walker from "../../public/images/walker.jpg";
+import { Dialog, DialogTrigger, DialogContent, DialogOverlay, DialogPortal } from "@radix-ui/react-dialog";
+import NextImage from "next/image";
+import NextFutureImage from "next/future/image";
+import React from "react";
+import { AnimatePresence, motion } from "framer-motion"
+import { cx } from "class-variance-authority"
 
 const images = [
   {
@@ -24,6 +30,7 @@ const images = [
     key: 3,
     src: lookup,
     alt: "Looking up at golden lights in Toronto's financial district",
+    vertical: true,
   },
   {
     key: 4,
@@ -45,6 +52,7 @@ const images = [
     src: tower_clouds,
     position: "top",
     alt: "The CN Tower appearing above the clouds",
+    vertical: true,
   },
   {
     key: 8,
@@ -53,22 +61,73 @@ const images = [
   },
 ];
 
+
+const Image2 = React.forwardRef(({ src, alt, ...props }, forwardRef) => (
+    <NextImage
+      src={src}
+      alt={alt}
+      ref={forwardRef}
+      layout="fill"
+      objectFit="cover"
+      className={"transition duration-500 betterhover:hover:scale-[1.03]"}
+      {...props}
+    />
+));
+
+  Image2.displayName = "Image2"
+
 const PhotoGrid = () => {
+  const [open, setOpen] = React.useState(false);
   return (
     <>
       <div className="grid h-auto grid-cols-2 sm:grid-cols-6 gap-yeat sm-g">
-        {images.map((image) => (
-          <Image
-            src={image.src}
-            className="photo-grid-item"
-            key={image.key}
-            alt={image.alt}
-            objectPosition={image.position}
-            quality={50}
-            priority
-            // placeholder="blur"
-          />
-        ))}
+        {images.map((image) => {
+          return (
+          <Dialog key={image.key}>
+            <DialogTrigger
+              className="relative w-full overflow-hidden transition-all duration-500 border rounded-lg shadow betterhover:hover:shadow-xl betterhover:hover:shadow-yolk/50 betterhover:hover:border-yolk border-stone photo-grid-item"
+              onClick={() => setOpen(true)}
+            >
+              <Image2
+                src={image.src}
+                alt={image.alt}
+                objectPosition={image.position}
+                quality={50}
+                priority
+                placeholder="blur"
+              />
+            </DialogTrigger>
+            
+              <DialogPortal>
+                <DialogOverlay className="fixed inset-0 bg-black/75 backdrop-blur-md">
+                </DialogOverlay>
+                <DialogContent className="fixed inset-0 mx-auto my-auto rounded shadow outline-none h-fit w-fit">
+                  <motion.div
+                    className={cx(
+                      "relative w-auto sm:h-[800px]",
+                      image.vertical ? "h-[500px]" : "h-[250px]"
+                    )}
+                    initial={{ y: 10, opacity: 0 }}
+                    animate={{
+                      opacity: 1,
+                      y: 0,
+                    }}
+                    transition={{
+                      duration: 0.4,
+                    }}
+                  >
+                    <NextFutureImage
+                      src={image.src}
+                      alt={image.alt}
+                      className="w-auto h-full rounded shadow-md"
+                      placeholder="blur"
+                    />
+                  </motion.div>
+                </DialogContent>
+              </DialogPortal>
+            
+          </Dialog>
+        )})}
       </div>
       <div className="mt-yeat"></div>
       {/* To do */}
