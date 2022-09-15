1ee8d6271ad0618cbc44b173920a8b86d9953806
Mitul Shah <mitulxshah@gmail.com>
2022-09-14 23:49:18 -0400

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -7,7 +7,6 @@ import React from "react";
 export default function Layout({ children }) {
   const { open } = React.useContext(SheetContext);
 
-  // There's a weird bug on about page where height of guiding lines doesn't become full due to translate?
   return (
     <div className="relative h-full">
       <Navigation />
