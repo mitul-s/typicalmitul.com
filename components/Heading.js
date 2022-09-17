20c9a339bca37bb79d18f689455f76ece4be19f9
Mitul Shah <mitulxshah@gmail.com>
2022-09-16 23:44:47 -0400

    Update Heading.js

--- components/Heading.js
+++ components/Heading.js
@@ -1,5 +1,7 @@
-const Heading = ({ children }) => (
-  <div className="flex items-center my-2 leading-none uppercase gap-x-2">
+import { cx } from "class-variance-authority";
+
+const Heading = ({ children, className }) => (
+  <div className={cx("flex items-center my-2 leading-none uppercase gap-x-2", className)}>
     <span className="w-2.5 h-2.5 rounded bg-yolk border border-dark -mt-0.5" />
     <h2 className="text-dark">{children}</h2>
   </div>
