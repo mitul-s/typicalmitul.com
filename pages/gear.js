1b38cee58db42f1b032b7fafe5354626d59035d8
Mitul Shah <mitulxshah@gmail.com>
2022-09-19 00:34:05 -0400

    adding some content

--- pages/gear.js
+++ pages/gear.js
@@ -3,6 +3,8 @@ import { NextSeo } from "next-seo";
 import NextImage from "next/future/image"
 import { Heading } from "../components";
 import sonyA7C from "../public/images/gear/sony-alpha-7c-full-frame-camera.png";
+import canon1635 from "../public/images/gear/Canon-EF-16-35mm-f4L-IS-USM.png";
+import jay_lookup from "../public/images/blue-jays-hat-lookup.jpg";
 import Footer from "@/components/sections/Footer";
 
 const gear_data = {
@@ -16,7 +18,7 @@ const gear_data = {
       shortDesc: "Something something something something something something",
       description:
         "The Sony A7C has incredible autofocus so you never miss a shot, and as with any Sony camera, you can shoot the ISO through the roof without a sweat. It has a great rangefinder inspired design, and although the viewfinder is on the smaller side, I’ve never had an issue with it.",
-      href: "https://amzn.to/3DtKWFa",
+      href: "https://amzn.to/3SczhyS",
       image: sonyA7C,
       imageAlt: "",
       tags: ["Sony", "Full-Frame", "Mirrorless", "Interchangeable-Lens"],
@@ -30,42 +32,47 @@ const gear_data = {
       subtitle: "Versatile, sharp and fast. What more can you ask for?",
       shortDesc:
         "This lens stays on my camera 90% of the time. It's has the best performance and offers high resolution throughout the entire zoom range.",
-      description:
-        "The Sony A7C has incredible autofocus so you never miss a shot, and as with any Sony camera, you can shoot the ISO through the roof without a sweat. It has a great rangefinder inspired design, and although the viewfinder is on the smaller side, I’ve never had an issue with it.",
-      href: "https://amzn.to/3DtKWFa",
+      href: "https://amzn.to/3SadLv7",
       image: sonyA7C,
       imageAlt: "",
       tags: ["Sony", "Full-Frame", "Mirrorless", "Interchangeable-Lens"],
     },
     {
       heading: "Lens",
-      title: "Sigma 24-70mm f/2.8",
-      longTitle: "Sigma 24-70mm f/2.8 DG DN Art Lens for Sony E-Mount Cameras",
-      subtitle: "Versatile, sharp and fast. What more can you ask for?",
+      title: "Canon 16-35mm f/4",
+      longTitle: "Canon EF 16-35mm f/4L IS USM Lens",
       shortDesc:
-        "This lens stays on my camera 90% of the time. It's has the best performance and offers high resolution throughout the entire zoom range.",
-      description:
-        "The Sony A7C has incredible autofocus so you never miss a shot, and as with any Sony camera, you can shoot the ISO through the roof without a sweat. It has a great rangefinder inspired design, and although the viewfinder is on the smaller side, I’ve never had an issue with it.",
-      href: "https://amzn.to/3DtKWFa",
-      image: sonyA7C,
+        "A wide angle lens is a must-have when shooting architecture or landscapes. I've taken my most popular photos with this lens.",
+      href: "https://amzn.to/3dm7s8m",
+      image: canon1635,
       imageAlt: "",
       tags: ["Sony", "Full-Frame", "Mirrorless", "Interchangeable-Lens"],
     },
-    {
-      heading: "Lens",
-      title: "Sigma 24-70mm f/2.8",
-      longTitle: "Sigma 24-70mm f/2.8 DG DN Art Lens for Sony E-Mount Cameras",
-      subtitle: "Versatile, sharp and fast. What more can you ask for?",
-      shortDesc:
-        "This lens stays on my camera 90% of the time. It's has the best performance and offers high resolution throughout the entire zoom range.",
-      description:
-        "The Sony A7C has incredible autofocus so you never miss a shot, and as with any Sony camera, you can shoot the ISO through the roof without a sweat. It has a great rangefinder inspired design, and although the viewfinder is on the smaller side, I’ve never had an issue with it.",
-      href: "https://amzn.to/3DtKWFa",
-      image: sonyA7C,
-      imageAlt: "",
-      tags: ["Sony", "Full-Frame", "Mirrorless", "Interchangeable-Lens"],
-    },
+    // {
+    //   heading: "Lens",
+    //   title: "Sigma 35mm f/1.4",
+    //   longTitle: "Sigma 35mm f/1.4 DG DN Art Lens for Sony E-Mount Cameras",
+    //   shortDesc:
+    //     "I’ve never been one for prime lenses, but having a 35mm has been essential for street photography or for when I’m trying to pack light. The depth of field from the f/1.8 is absolutely beautiful.",
+    //   href: "",
+    //   image: sonyA7C,
+    //   imageAlt: "",
+    //   tags: ["Sony", "Full-Frame", "Mirrorless", "Interchangeable-Lens"],
+    // },
   ],
+  drone: {
+    heading: "Drone",
+    title: "DJI Mavic Air 2",
+    longTitle: "DJI Mavic Air 2",
+    subtitle: "The best drone for the price.",
+    shortDesc: "The best drone for the price.",
+    description:
+      "The DJI Mavic Air 2 is the best drone for the price. It has a 48MP camera, 34 minutes of flight time, and a 4K video recording capability. It’s also super easy to fly and has a great range.",
+    href: "https://amzn.to/3Sb9Z9M",
+    image: sonyA7C,
+    imageAlt: "",
+    tags: ["DJI", "Mavic", "Air", "2"],
+  },
 };
 
 
@@ -126,14 +133,24 @@ const GearItem = ({ heading, image, title, subtitle, href, description, imageAlt
     );
 }
 
-const GearItemSmall = ({ shortDesc, title, }) => {
+const GearItemSmall = ({ image, imageAlt, shortDesc, title, }) => {
   return (
-    <div>
+    <div className="flex flex-col">
       {/* <span className="text-xs uppercase text-stone">Camera Body</span> */}
-      <div className="bg-white border rounded aspect-square border-stone-light"></div>
-      <h3 className="mt-4 text-xl font-mtl-bold">{title}</h3>
-      <p className="mt-0.5 mb-4 text-xl max-w-prose text-stone-blue">{shortDesc}</p>
-      <a className="inline-block w-full p-4 text-center rounded bg-yolk">
+      <div className="grid bg-white border rounded aspect-square border-stone-light place-content-center">
+        <div className="w-fit">
+          <NextImage
+            src={image}
+            className="w-1/2 mx-auto drop-shadow-md"
+            alt={imageAlt}
+          />
+        </div>
+      </div>
+      <h3 className="mt-4 text-sm md:text-xl font-mtl-bold">{title}</h3>
+      <p className="mt-0.5 mb-4 text-sm md:text-xl max-w-prose text-stone-blue">
+        {shortDesc}
+      </p>
+      <a className="inline-block w-full p-4 mt-auto text-center rounded bg-yolk">
         Buy now
       </a>
     </div>
@@ -171,7 +188,7 @@ const Gear = () => {
               );
             })}
             <Break />
-            <div className="grid grid-cols-3 gap-x-6">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-yeat md:gap-x-6">
               <Heading className="col-span-full">Lenses</Heading>
               {gear_data.lenses.map((item) => {
                 return (
@@ -187,12 +204,19 @@ const Gear = () => {
                   />
                 );
               })}
+              {/* <GearItemSmall /> */}
+              <div className="overflow-hidden bg-white border rounded">
+                <NextImage src={jay_lookup} 
+                  className="rounded"
+                  style={{ objectFit: "cover", height: "100%" }}
+                />
+              </div>
             </div>
           </section>
           <Break />
           <section>
             <div className="grid mx-4 mt-12 border rounded bg-yolk/10 h-96 border-yolk place-content-center">
-              See the photos I've taken with this equipment
+              See the photos I&apos;ve taken with this equipment
             </div>
           </section>
         </main>
