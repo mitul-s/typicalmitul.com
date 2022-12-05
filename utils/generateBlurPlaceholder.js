8b5a50095f97be1642cc28b7a9cefd3ecf1ce8ef
Mitul Shah <mitulxshah@gmail.com>
2022-12-04 23:55:04 -0500

    utils

---
+++ utils/generateBlurPlaceholder.js
@@ -0,0 +1,18 @@
+const cache = new Map()
+
+export default async function getBase64ImageUrl(
+  image
+){
+  let url = cache.get(image)
+  if (url) {
+    return url
+  }
+  const response = await fetch(
+    `https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/f_jpg,w_8,q_70/${image.public_id}.${image.format}`
+  )
+  const buffer = await response.arrayBuffer()
+  const base64 = Buffer.from(buffer).toString('base64')
+  url = `data:image/jpeg;base64,${base64}`
+  cache.set(image, url)
+  return url
+}
