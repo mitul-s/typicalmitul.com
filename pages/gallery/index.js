4e63a004095dcd55e5d1469518f9e1e1a2106ea1
Mitul Shah <mitulxshah@gmail.com>
2022-12-08 23:46:46 -0500

    cleanup

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,12 +1,12 @@
 import { useRouter } from "next/router";
 import cloudinary from "utils/cloudinary";
-import Link from "@/components/Link";
+import { useState } from "react";
+import Link from "next/link";
 import Image from "next/future/image";
 import getBase64ImageUrl from "utils/generateBlurPlaceholder";
 import Masonry from "react-masonry-css";
 import {
   Dialog,
-  DialogTrigger,
   DialogOverlay,
   DialogContent,
   DialogPortal,
@@ -15,6 +15,11 @@ import {
 const Gallery = ({ images }) => {
   const router = useRouter();
   const { photoId } = router.query;
+  const [open, setOpen] = useState(false);
+  const [selectedImage, setSelectedImage] = useState({
+    public_id: "",
+    format: "",
+  });
 
   const breakpointColumnsObj = {
     default: 4,
@@ -24,47 +29,61 @@ const Gallery = ({ images }) => {
   };
 
   return (
-    <main>
-      <div className="">
-        <Dialog>
-          <DialogTrigger>Click me</DialogTrigger>
+    <main className="relative">
+      <Dialog open={open} onOpenChange={setOpen}>
+        {photoId && (
           <DialogPortal>
             <DialogOverlay className="fixed inset-0 bg-black/75 backdrop-blur-md rdx-state-open:overlay-fade-in rdx-state-closed:overlay-fade-out" />
             <DialogContent
               className="fixed inset-0 mx-auto my-auto rounded shadow outline-none h-fit w-fit rdx-state-open:dialog-item-open rdx-state-closed:dialog-item-close"
-              onCloseAutoFocus={(e) => e.preventDefault()}
+              onEscapeKeyDown={() => router.push("/gallery")}
+              onPointerDownOutside={() => router.push("/gallery")}
             >
-              content
-            </DialogContent>
-          </DialogPortal>
-        </Dialog>
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="my-masonry-grid"
-          columnClassName="my-masonry-grid_column"
-        >
-          {images.map(
-            ({ id, public_id, format, width, height, blurDataUrl }) => (
-              <Link
-                key={id}
-                href="/gallery/[id]"
-                as={`/gallery/${id}`}
-                shallow
-                className="overflow-hidden transition-all duration-500 border rounded-lg shadow betterhover:hover:shadow-xl betterhover:hover:shadow-yolk/50 betterhover:hover:border-yolk border-stone"
-              >
+              <div className="relative w-auto sm:h-[800px]">
                 <Image
                   alt=""
-                  placeholder="blur"
-                  blurDataURL={blurDataUrl}
-                  src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
-                  width={width}
-                  height={height}
+                  src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_1440/${selectedImage.public_id}.${selectedImage.format}`}
+                  fill
+                  className="w-auto h-full rounded shadow-md !relative"
                 />
-              </Link>
-            )
-          )}
-        </Masonry>
-      </div>
+              </div>
+            </DialogContent>
+          </DialogPortal>
+        )}
+      </Dialog>
+      <Masonry
+        breakpointCols={breakpointColumnsObj}
+        className="my-masonry-grid"
+        columnClassName="my-masonry-grid_column"
+      >
+        {images.map(({ id, public_id, format, width, height, blurDataUrl }) => (
+          <Link
+            key={id}
+            href={`/gallery/?photoId=${id}`}
+            as={`/gallery/${id}`}
+            shallow
+          >
+            <Image
+              onClick={() => {
+                setSelectedImage({
+                  public_id: public_id,
+                  format: format,
+                  alt: "",
+                  blurDataURL: blurDataUrl,
+                });
+                setOpen(true);
+              }}
+              className="block overflow-hidden transition-all duration-500 border rounded-lg shadow betterhover:hover:shadow-xl betterhover:hover:shadow-yolk/50 betterhover:hover:border-yolk border-stone"
+              alt=""
+              placeholder="blur"
+              blurDataURL={blurDataUrl}
+              src={`https://res.cloudinary.com/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload/c_scale,w_720/${public_id}.${format}`}
+              width={width}
+              height={height}
+            />
+          </Link>
+        ))}
+      </Masonry>
     </main>
   );
 };
