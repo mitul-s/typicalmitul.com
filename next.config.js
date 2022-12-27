12222c32197ec4d1dba1934c4dc57937bed24504
Mitul Shah <mitulxshah@gmail.com>
2022-12-27 18:41:34 -0500

    create re-routes

--- next.config.js
+++ next.config.js
@@ -15,6 +15,26 @@ module.exports = {
         source: "/gallery/concerts",
         destination: "/gallery?type=concerts",
       },
+      {
+        source: "/gallery/architecture",
+        destination: "/gallery?type=architecture",
+      },
+      {
+        source: "/gallery/street",
+        destination: "/gallery?type=street",
+      },
+      {
+        source: "/gallery/nature",
+        destination: "/gallery?type=nature",
+      },
+      {
+        source: "/gallery/outdoors",
+        destination: "/gallery?type=outdoors",
+      },
+      {
+        source: "/gallery/all",
+        destination: "/gallery?type=all",
+      },
     ];
   },
 };
