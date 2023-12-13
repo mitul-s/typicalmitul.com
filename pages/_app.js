2009bf6993a2858fefb5caa0ae70e5de5fb71a91
Mitul Shah <mitulxshah@gmail.com>
2023-12-13 16:01:14 -0500

    Revert "add speedisnights"
    This reverts commit e948ffee3a9b87482500b8c4b06d48a9ee31f75c.

--- pages/_app.js
+++ pages/_app.js
@@ -1,25 +1,24 @@
-import "../styles/fonts.css";
-import "../styles/globals.css";
+import '../styles/fonts.css'
+import '../styles/globals.css'
 
 import React, { useEffect } from "react";
 import { TooltipProvider } from "@radix-ui/react-tooltip";
-import { SheetProvider } from "@/components/Sheet";
+import { SheetProvider } from "@/components/Sheet"
 import { DefaultSeo } from "next-seo";
-import Layout from "@/components/Layout";
+import Layout from "@/components/Layout"
 
 import splitbee from "@splitbee/web";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const MyApp = ({ Component, pageProps }) => {
+
   useEffect(() => {
     splitbee.init();
-  }, []);
+  }, [])
 
   return (
     <TooltipProvider>
       <SheetProvider>
         <Layout>
-          <SpeedInsights />
           <DefaultSeo
             title={undefined}
             titleTemplate="%s | Typical Mitul"
@@ -36,8 +35,7 @@ const MyApp = ({ Component, pageProps }) => {
               locale: "en_CA",
               url: "https://typicalmitul.com",
               title: "Typical Mitul | Toronto Photographer for Hire",
-              description:
-                "Photographer based in Toronto, Canada specializing in cityscapes, architecture and portraits.",
+              description: "Photographer based in Toronto, Canada specializing in cityscapes, architecture and portraits.",
               images: [
                 {
                   url: "https://typicalmitul.com/images/og.png",
@@ -45,8 +43,8 @@ const MyApp = ({ Component, pageProps }) => {
                   height: 630,
                   alt: "Typical Mitul | Toronto Photographer for Hire",
                   type: "image/png",
-                },
-              ],
+                }
+              ]
             }}
           />
           <Component {...pageProps} />
@@ -54,6 +52,6 @@ const MyApp = ({ Component, pageProps }) => {
       </SheetProvider>
     </TooltipProvider>
   );
-};
+}
 
-export default MyApp;
+export default MyApp
